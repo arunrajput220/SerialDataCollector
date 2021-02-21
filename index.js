86b2afc001e45bfec587dce3018cd610844d78cc
arunrajput220 <50443428+arunrajput220@users.noreply.github.com>
2021-02-21 13:15:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,43 +14,10 @@ parser.on('open', function () {
 
 
 parser.on('data', function (data) {
-    sendTemperatureData(data.slice(0, 5))
-    sendHumidityData(data.slice(6, 11))
+    console.log(data);
   })
 
-function sendTemperatureData(tempval){
-  var request = require('request');
-var options = {
-  'method': 'PUT',
-  'url': 'https://iotapp-aad4a.firebaseio.com/temperature.json',
-  'headers': {
-    'Content-Type': 'application/json'
-  },
-  body: tempval
 
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
-}
-
-function sendHumidityData(humidval){
-  var request = require('request');
-  var options = {
-  'method': 'PUT',
-  'url': 'https://iotapp-aad4a.firebaseio.com/humidity.json',
-  'headers': {
-    'Content-Type': 'application/json'
-  },
-  body: humidval
-
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
-}
 
 
 
